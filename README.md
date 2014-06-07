fce9e0dd4d2dcbe62f3df6088706f19b5148719c
Anthony Santarosa <anthony@guhru.com>
2014-06-07 12:05:50 -0400

    Initial Commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 underscores-responsive
 ======================
 
-WordPress starter template using Automattic's Underscores Starter Theme and Dennis Leblanc's Responsive Grid System
+WordPress starter template using Automattic's Underscores Starter Theme and Dennis Leblanc's Responsive Grid System.
+
+Credit to:
+
+Automattic: http://underscores.me/
+
+Dennis Leblanc: http://responsive.gs/
+
+Scott Jehl: http://j.mp/respondjs
+
+
+<!-- How the grid system works -->
+
+<div class="container"></div>
+<!-- Add this class to your page wrapper to center it to the browser window and set the width of the page. -->
+
+<div class="row"></div>
+<!-- Add this class to any element that spans the entire width of your 'container' to clear any floating child or 'col' elements. Also allows you to define spacing between rows, or add borders, etc. Using this class alone does not add gutters to 'col' elements. -->
+
+<div class="row gutters"></div>
+<!-- Using these two classes together works the same as described above and adds gutters to 'col' elements within. If you don't need gutters then just use the 'row' class on its own. -->
+
+<div class="col span_4"></div>
+<!-- The 'col' class defines each column within a 'row' and the 'span_' classes define the width of each particular column. These are used together as child elements of a 'row'. On smaller devices these elements will stack vertically unless defined otherwise in media queries. -->
+
+<div class="clr"></div>
+<!-- Add this class to any element to clear its floating children. Also, 'container', 'row' and 'col' will clear floats by default. (aka - clearfix method) -->
---
+++ inc/template-tags.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ * Custom template tags for this theme.
+ *
+ * Eventually, some of the functionality here could be replaced by core features.
+ *
+ * @package Underscores Responsive
+ */
+
+if ( ! function_exists( 'urs_paging_nav' ) ) :
+/**
+ * Display navigation to next/previous set of posts when applicable.
+ */
+function urs_paging_nav() {
+	// Don't print empty markup if there's only one page.
+	if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+		return;
+	}
+	?>
+	<nav class="navigation paging-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'urs' ); ?></h1>
+		<div class="nav-links">
+
+			<?php if ( get_next_posts_link() ) : ?>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'urs' ) ); ?></div>
+			<?php endif; ?>
+
+			<?php if ( get_previous_posts_link() ) : ?>
+			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'urs' ) ); ?></div>
+			<?php endif; ?>
+
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
+endif;
+
+if ( ! function_exists( 'urs_post_nav' ) ) :
+/**
+ * Display navigation to next/previous post when applicable.
+ */
+function urs_post_nav() {
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+
+	if ( ! $next && ! $previous ) {
+		return;
+	}
+	?>
+	<nav class="navigation post-navigation" role="navigation">
+		<h1 class="screen-reader-text"><?php _e( 'Post navigation', 'urs' ); ?></h1>
+		<div class="nav-links">
+			<?php
+				previous_post_link( '<div class="nav-previous">%link</div>', _x( '<span class="meta-nav">&larr;</span> %title', 'Previous post link', 'urs' ) );
+				next_post_link(     '<div class="nav-next">%link</div>',     _x( '%title <span class="meta-nav">&rarr;</span>', 'Next post link',     'urs' ) );
+			?>
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
+endif;
+
+if ( ! function_exists( 'urs_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ */
+function urs_posted_on() {
+	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$posted_on = sprintf(
+		_x( 'Posted on %s', 'post date', 'urs' ),
+		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		_x( 'by %s', 'post author', 'urs' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	);
+
+	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+
+}
+endif;
+
+/**
+ * Returns true if a blog has more than 1 category.
+ *
+ * @return bool
+ */
+function urs_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'urs_categories' ) ) ) {
+		// Create an array of all the categories that are attached to posts.
+		$all_the_cool_cats = get_categories( array(
+			'fields'     => 'ids',
+			'hide_empty' => 1,
+
+			// We only need to know if there is more than one category.
+			'number'     => 2,
+		) );
+
+		// Count the number of categories that are attached to the posts.
+		$all_the_cool_cats = count( $all_the_cool_cats );
+
+		set_transient( 'urs_categories', $all_the_cool_cats );
+	}
+
+	if ( $all_the_cool_cats > 1 ) {
+		// This blog has more than 1 category so urs_categorized_blog should return true.
+		return true;
+	} else {
+		// This blog has only 1 category so urs_categorized_blog should return false.
+		return false;
+	}
+}
+
+/**
+ * Flush out the transients used in urs_categorized_blog.
+ */
+function urs_category_transient_flusher() {
+	// Like, beat it. Dig?
+	delete_transient( 'urs_categories' );
+}
+add_action( 'edit_category', 'urs_category_transient_flusher' );
+add_action( 'save_post',     'urs_category_transient_flusher' );
